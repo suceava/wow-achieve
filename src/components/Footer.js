bc5385470b2e4ce0060fda8e2ddf2138b76c6bdc
Dan Suceava <dan.suceava@gmail.com>
2018-03-01 18:32:33 -0700

    add email in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,12 @@ class Footer extends Component {
   render() {
     return(
       <div className="footer">
-        Built on {new Date(apv.build.date).toUTCString()}
+        <div className="footer-feedback">
+          Feedback welcome: feedback@gnarlybits.com
+        </div>
+        <div className="footer-build">
+          Built on {new Date(apv.build.date).toUTCString()}
+        </div>
       </div>
     );
   }
